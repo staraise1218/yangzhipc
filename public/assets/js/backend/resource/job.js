4a0ac3ecaa45b710cefd653d9808ca09f65b7c36
shengli <363912809@qq.com>
2019-01-20 10:32:07 +0800

    bug

--- public/assets/js/backend/resource/job.js
+++ public/assets/js/backend/resource/job.js
@@ -16,6 +16,7 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
 
             var table = $("#table");
 
+
             // 初始化表格
             table.bootstrapTable({
                 url: $.fn.bootstrapTable.defaults.extend.index_url,
