6c504d72e8f72925f9b0d2b0a8ef962e63135164
shengli <363912809@qq.com>
2019-08-13 09:47:46 +0800

    bg

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -21,9 +21,16 @@ class Index extends Frontend
     public function index()
     {
         $adList = Db::name('ad')
-            ->where('ad_position_id', 1)
+            ->where('ad_position_id', 'in', [1, 3])
             ->field('title, image, link')
             ->select();
+
+        foreach ($adList as $item) {
+            // 轮播图
+            if($item['ad_position_id'] == 1) $bannerList[] = $item;
+            // 合作伙伴
+            if($item['ad_position_id'] == 3) $parterList[] = $item;
+        }
 
         // 行业知识
         $knowledge = Db::name('knowledge')->order('id desc')->limit(5)->select();
@@ -31,8 +38,10 @@ class Index extends Frontend
         $project = Db::name('project')->where('is_delete', 0)->order('id desc')->limit(5)->select();
          // 行业知识
         $exhibition = Db::name('exhibition')->order('id desc')->limit(5)->select();
+        // 
 
-        $this->assign('adList', $adList);
+        $this->assign('bannerList', $bannerList);
+        $this->assign('parterList', $parterList);
         $this->assign('knowledge', $knowledge);
         $this->assign('project', $project);
         $this->assign('exhibition', $exhibition);
